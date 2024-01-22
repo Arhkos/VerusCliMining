2dc3de760cecb4eed0664c2a6ce4e8a281b80555
Arhkos <87340721+Arhkos@users.noreply.github.com>
2024-01-22 21:47:22 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,11 +3,11 @@
 echo "Update du config.json"
 read  -p "Nom du mineur = " MINERNAME
 wget https://raw.githubusercontent.com/Arhkos/VerusCliMining/main/config.json -O ~/ccminer/config.json
-echo "config.json copié"
+echo "config.json copi  "
 echo "Update de la config pour $MINERNAME"
-sed '9s/SG10e/$MINERNAME/' ccminer/config.json
+sed '9s/SG10e/'$MINERNAME'/' ccminer/config.json
 echo "Update du script autostartmining"
 wget https://raw.githubusercontent.com/Arhkos/VerusCliMining/main/autostartmining.sh -O /etc/profile.d/autostartmining.sh
-echo "autostartmining copié"
+echo "autostartmining ok "
 
-echo "Update terminée"
+echo "Update ok"
